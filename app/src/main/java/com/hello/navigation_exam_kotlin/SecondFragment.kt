ed1d717fb1f81b42ce83187a47ae4c9dffef7002
peterlee <foxworld@gmail.com>
2024-05-21 14:12:06 +0900

    safe args 추가

--- app/src/main/java/com/hello/navigation_exam_kotlin/SecondFragment.kt
+++ app/src/main/java/com/hello/navigation_exam_kotlin/SecondFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.fragment.navArgs
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +22,8 @@ class SecondFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    val args: SecondFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,7 +64,7 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val text = SecondFragmentArgs.fromBundle(requireArguments()).value
-        view.findViewById<TextView>(R.id.textView).text = text
+//        val text = SecondFragmentArgs.fromBundle(requireArguments()).value
+        view.findViewById<TextView>(R.id.textView).text = args.value
     }
 }
